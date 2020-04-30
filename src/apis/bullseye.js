bff7dff35cbce389a7b52ad0581dad3466d378d1
iamthe-Wraith <allhailthewraith@protonmail.com>
2020-04-30 08:47:02 -0400

    updated bullseye axios instance to pull token from localstorage

--- src/apis/bullseye.js
+++ src/apis/bullseye.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const token = window.localStorage.getItem('bullseyeAuth');
+
+const headers = { serviceName: 'bullseye' };
+if (token) headers.Authorization = token;
+
 export const BullsEyeAPI = axios.create({
   baseURL: 'https://bb-bullseye-api.herokuapp.com',
-  headers: {
-    serviceName: 'bullseye'
-  }
+  headers
 });

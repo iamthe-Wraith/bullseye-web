a10424c231b7653c249920461927b0532b7e5ad7
iamthe-Wraith <allhailthewraith@protonmail.com>
2020-04-30 11:24:08 -0400

    added axios interceptors to manage token automatically

--- src/apis/bullseye.js
+++ src/apis/bullseye.js
@@ -1,11 +1,35 @@
 import axios from 'axios';
 
-const token = window.localStorage.getItem('bullseyeAuth');
-
 const headers = { serviceName: 'bullseye' };
-if (token) headers.Authorization = token;
 
-export const BullsEyeAPI = axios.create({
+const BullsEyeAPI = axios.create({
+  headers,
   baseURL: 'https://bb-bullseye-api.herokuapp.com',
-  headers
 });
+
+BullsEyeAPI.interceptors.request.use(config => {
+  const updatedConfig = { ...config };
+  const token = window.localStorage.getItem('bullseyeAuth');
+
+  if (token) updatedConfig.headers.Authorization = token;
+
+  return updatedConfig;
+}, err => {
+  return Promise.reject(err);
+})
+
+BullsEyeAPI.interceptors.response.use(response => {
+  const authToken = response?.headers?.authorization;
+  
+  if (authToken) {
+    window.localStorage.setItem('bullseyeAuth', authToken);
+  } else {
+    window.localStorage.removeItem('bullseyeAuth');
+  }
+
+  return response;
+}, err => {
+  return Promise.reject(err);
+});
+
+export default BullsEyeAPI
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BullsEyeAPI } from '../apis/bullseye';
+import BullsEyeAPI from '../apis/bullseye';
 
 import './App.scss';
 
